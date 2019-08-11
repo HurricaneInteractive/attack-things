14392eb9ae958f5a73a23769fe7af578243d9967
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-08-11 20:19:09 +1000

    Expanding on item class to pass in mutation method

--- src/class/Item.ts
+++ src/class/Item.ts
@@ -1,9 +1,15 @@
+import { Mutate } from "@/types/mutations";
+
 class Item {
   public name: string;
+  public mutates: Mutate;
 
-  public constructor(name: string) {
+  public constructor(name: string, mutates: Mutate) {
     this.name = name;
+    this.mutates = mutates;
   }
 }
+
+const Potion = new Item("Potion", "UPDATE_PLAYER_HP");
 
 export default Item;
--- src/types/mutations.ts
+++ src/types/mutations.ts
@@ -1,4 +1,7 @@
 export enum MUTATIONS {
   UPDATE_PLAYER_NAME = "update_player_name",
-  UPDATE_PLAYER_STAT = "update_player_stat"
+  UPDATE_PLAYER_STAT = "update_player_stat",
+  UPDATE_PLAYER_HP = "update_player_hp"
 }
+
+export type Mutate = keyof typeof MUTATIONS;
