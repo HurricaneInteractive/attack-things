a03ee34f423acbeb3666f35f1993d0bdc04eb0d7
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-08-12 15:23:11 +1000

    Add item price to class

--- src/class/Item.ts
+++ src/class/Item.ts
@@ -3,13 +3,13 @@ import { Mutate } from "@/types/mutations";
 class Item {
   public name: string;
   public mutates: Mutate;
+  public price: number;
 
-  public constructor(name: string, mutates: Mutate) {
+  public constructor(name: string, mutates: Mutate, price: number) {
     this.name = name;
     this.mutates = mutates;
+    this.price = price;
   }
 }
-
-const Potion = new Item("Potion", "UPDATE_PLAYER_HP");
 
 export default Item;
