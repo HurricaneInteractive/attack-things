a050f90372c409b0a53008103455f8280171cf3f
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-08-12 21:25:49 +1000

    WIP: InventoryList component

---
+++ src/components/player/InventoryList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Inventory from '@/class/Inventory';
+
+export default Vue.extend({
+  name: "InventoryList",
+  computed: {
+    inventory(): Inventory {
+      return this.$store.state.player.inventory;
+    }
+  }
+})
+</script>
