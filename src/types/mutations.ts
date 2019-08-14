0b808d51b54546d17d842937d8cdd4e0311c8705
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-08-14 21:15:55 +1000

    Added update_player_mp mutation type

--- src/types/mutations.ts
+++ src/types/mutations.ts
@@ -1,7 +1,8 @@
 export enum MUTATIONS {
   UPDATE_PLAYER_NAME = "update_player_name",
   UPDATE_PLAYER_STAT = "update_player_stat",
-  UPDATE_PLAYER_HP = "update_player_hp"
+  UPDATE_PLAYER_HP = "update_player_hp",
+  UPDATE_PLAYER_MP = "update_player_mp"
 }
 
 export type Mutate = keyof typeof MUTATIONS;
